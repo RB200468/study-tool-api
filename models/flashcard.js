0c04a869c4cb5fbb5aa872fdf9d4493b65c0e8e1
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-05 14:11:30 +0100

    add database schema models

---
+++ models/flashcard.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const flashcardSchema = new mongoose.Schema({
+  term: { type: String, required: true },
+  definition: { type: String, required: true }
+});
+
+module.exports = flashcardSchema;
