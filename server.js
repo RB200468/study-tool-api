1207e1fda866dcedd73247aa6bde4e59d2235420
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-05 13:49:40 +0100

    add basic router functionality for users

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const portNumber = 5001
-
 const mongoose = require('mongoose')
 
+// Database connection
 mongoose.connect(process.env.DB_CONNECTION_STRING)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
+app.use(express.json())
+
+// Routers
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
+
+
 
 app.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2", "user3"]});
