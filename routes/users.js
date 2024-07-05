6119facd048517bbc3c5c5cbfb5458dc5fbee73c
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-05 14:52:34 +0100

    add middleware to routes, implement simple add user logic

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,43 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const getUser = require('../middleware/getUser')
 
 // getting all
-router.get('/', (req, res) => {
-    res.send('hello world')
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 // getting one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', getUser, (req, res) => {
+    res.send(res.user.username)
 })
 
 // creating one
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    try {
+        const { username, password_hash, email } = req.body;
+        const user = new User({ username, password_hash, email });
+        await user.save();
+        res.status(201).send('User registered');
+      } catch (error) {
+        res.status(400).send('Error registering user');
+      }
 })
 
 // updating one
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getUser, (req, res) => {
     
 })
 
 // deleting one
-router.delete('/:id', (req, res) => {
+router.delete('/:id', getUser, (req, res) => {
     
 })
-
-
-
-
 
 module.exports = router
