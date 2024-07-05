7cb2128a09ce120c7e5d44081bd4c1b0a525123a
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-05 21:40:58 +0100

    change server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 const portNumber = 5001
 const mongoose = require('mongoose')
 const connectDB = require('./config/connectDB.js')
+const cookieParser = require('cookie-parser')
+
 app.use(express.json())
+app.use(cookieParser())
 
 // Database connection
 connectDB()
