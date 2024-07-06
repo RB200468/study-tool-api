f1a3d50f9c25c19746ac2866eeb0fc28478964f9
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-06 12:36:13 +0100

    add deck routes file, add `get-all` route

---
+++ routes/decks.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bcrypt = require('bcrypt');
+const router = express.Router()
+const User = require('../models/user')
+const getUser = require('../middleware/getUser');
+const jwtAuth = require('../middleware/jwtAuth');
+
+// get all
+router.get('/', jwtAuth, async (req, res) => {
+    try {
+        const user = await User.findOne(req.user.id)
+
+        if (!user){
+            res.status(400).json({ message: "User not found" })
+        }
+
+        const decks = user.library.map(deck => ({
+            id: deck.deck_id,
+            name: deck.name
+        }))
+        
+        res.status(200).json(decks)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+// get one
+
+// create one
+
+// delete one
+
+
+module.exports = router
