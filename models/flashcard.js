8c54957601260dfec0290494958d6ec9ceb1a3bd
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-09 11:06:37 +0100

    add id field to flashcard model

--- models/flashcard.js
+++ models/flashcard.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const flashcardSchema = new mongoose.Schema({
-  term: { type: String, required: true },
-  definition: { type: String, required: true }
+    flashcard_id: { type: mongoose.Schema.Types.ObjectId, default: new mongoose.Types.ObjectId() },
+    term: { type: String, required: true },
+    definition: { type: String, required: true }
 });
 
 module.exports = flashcardSchema;
