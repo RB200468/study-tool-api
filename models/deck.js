4789789295c4a7006c5eaafc67190ab1782885e0
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-09 11:38:07 +0100

    refactor model structure for decks and flashcards

--- models/deck.js
+++ models/deck.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const flashcardSchema = require('./flashcard');
 
 const deckSchema = new mongoose.Schema({
-  deck_id: { type: mongoose.Schema.Types.ObjectId, default: new mongoose.Types.ObjectId() },
   name: { type: String, required: true },
   flashcards: [flashcardSchema]
 });
--- models/flashcard.js
+++ models/flashcard.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const flashcardSchema = new mongoose.Schema({
-    flashcard_id: { type: mongoose.Schema.Types.ObjectId, default: new mongoose.Types.ObjectId() },
     term: { type: String, required: true },
     definition: { type: String, required: true }
 });
