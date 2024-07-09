931f6cd5fa8414d7294bfe79a49d3c67a39a8b02
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-09 22:55:16 +0100

    add delete flashcard route

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -32,4 +32,32 @@ router.post('/:id', jwtAuth, async (req, res) => {
     }
 })
 
+// Delete a flashcard
+router.delete('/:id', jwtAuth, async (req,res) => {
+    try{
+        const user = await User.findById(req.user.id)
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+
+        const deck = user.library.id(req.params.id)
+        if (!deck) {
+            return res.status(404).json({ message: "Deck not found" })
+        }
+
+        const flashcard = deck.flashcards.id(req.body.flashcard_id)
+        if (!flashcard) {
+            return res.status(404).json({ message: "Flashcard not found" })
+        }
+
+        flashcard.deleteOne()
+        await user.save()
+
+        res.status(200).json({ message: "Flashcard created" })
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
 module.exports = router
