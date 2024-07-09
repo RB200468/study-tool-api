54b20c106e676ccb0fd856514853124590c43050
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-09 22:44:54 +0100

    add create flashcard route

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -1,7 +1,35 @@
 const express = require('express');
-const User = require('../models/user')
+const User = require('../models/user');
+const jwtAuth = require('../middleware/jwtAuth');
 const router = express.Router();
 
-// Get all given deck id
+
+// Create one given deck id, term and definition
+router.post('/:id', jwtAuth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id)
+        if (!user) {
+            res.status(404).json({ message: "User not found" })
+        }
+
+        const deck = user.library.id(req.params.id)
+        if (!deck) {
+            res.status(404).json({ message: "Deck not found" })
+        }
+
+        const flashCard = {
+            term: req.body.term,
+            definition: req.body.definition
+        }
+
+        deck.flashcards.push(flashCard)
+        await user.save()
+
+        res.status(200).json({ message: "Flashcard created" })
+
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 module.exports = router
