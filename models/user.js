85d0b88db763f2d259c8f27c1201a9a146685aa6
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-10 19:50:40 +0100

    add is_admin field in user model

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password_hash: { type: String, required: true },
+  is_admin: { type: Boolean, default: false, required: false },
   library: [deckSchema]
 });
 
