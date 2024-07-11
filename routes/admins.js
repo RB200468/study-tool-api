5e7a87a9b7a472a1abd6cbfe3935da4cd0cb68fd
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2024-07-11 17:48:47 +0100

    add change name of deck by user id and deck id route

--- routes/admins.js
+++ routes/admins.js
@@ -103,6 +103,24 @@ router.get('/users/:id/decks', jwtAuth, confirmUser, isAdmin, getUser, async (re
 })
 
 // Update any users deck name by user ID and deck ID
+router.patch('/users/:id/decks/:deck_id', checkReqBody, jwtAuth, confirmUser, isAdmin, getUser, async (req, res) => {
+    try {
+        const { name } = req.body
+
+        const deck = res.user.library.id(req.params.deck_id)
+        if (!deck) {
+            return res.status(404).json({ message: "Deck not found" })
+        }
+
+        if (name !== undefined) { deck.name = name }
+
+        await res.user.save();
+
+        res.status(200).json({ message: "User credentials patched" })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 // Delete any users deck by user ID and deck ID
 
